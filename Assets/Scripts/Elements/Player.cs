1a4bf12a045dfe1de855fad94de98f4cab38366e
Furkan Akyün <furkan_akyun@hotmail.com>
2024-10-23 19:47:22 +0300

    Player Movement Added

--- Assets/Scripts/Elements/Player.cs
+++ Assets/Scripts/Elements/Player.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float playerSpeed;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        Vector3 direction = Vector3.zero;
+        if(Input.GetKey(KeyCode.W))
+        {
+            direction += Vector3.up;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            direction += Vector3.down;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector3.right;
+        }
+        transform.position += direction * playerSpeed * Time.deltaTime;
     }
 }
